d42e8c0ada4653f2facd497be40bbe4586507300
Chihyung <khiiean.shin@gmail.com>
2024-06-28 14:11:47 +0900

    수정완료

--- cal_main.c
+++ cal_main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
-    printf("==> %d + %d = %d\n", a, b, add(a, b));
+    printf("==?s> %d + %d = %d\n", a, b, add(a, b));
     printf("==> %d - %d = %d\n", a, b, sub(a, b));
     printf("==> %d * %d = %d\n", a, b, mul(a, b));
     printf("==> %d / %d = %d\n", a, b, dur(a, b));
